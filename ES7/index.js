98e218c9e4daabf07b389e718ca41af1ed55a4ec
AndresMiraQ <krhistian1902@gmail.com>
2021-10-30 16:14:20 -0500

    EcmaScript7 notes

---
+++ ES7/index.js
@@ -0,0 +1,13 @@
+//.includes()
+
+let letters = [a,b,d,e,g,h]
+
+if(letters.includes(c)){
+    console.log('If found');
+}else{
+    console.log('Is not found');//Is not found c
+}
+
+//Exponet operator
+
+let result = 2**2; // 4
